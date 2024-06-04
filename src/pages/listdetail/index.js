4ad4b5fe5b339ea94835b773c80e30a14e417ce0
sriramkrish68 <sriramkrish379@gmail.com>
2024-06-04 18:26:28 +0530

    added

--- src/pages/listdetail/index.js
+++ src/pages/listdetail/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Header } from '../../components/navbar';
-import { useLocation } from 'react-router-dom/dist';
+
 import Footer from '../../components/footer';
 import './style.css';
 import {
@@ -15,7 +15,7 @@ import {
 export const ListPageDetail = () => {
     const [algoApiData, setAlgoApiData] = useState({})
     let { id } = useParams();
-    const location = useLocation()
+   
     useEffect(() => {
         fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
             .then(res => res.json())
@@ -29,7 +29,28 @@ export const ListPageDetail = () => {
             <Header />
             <div className="list-page">
                 {algoApiData ? <h1 className="list-page__title">{algoApiData.title}</h1> : null}
-        
+                <ul>
+                    <a href={`https://whatsapp.com`}
+                        target={"_blank"} rel="noreferrer">
+                        <li><WhatsappIcon size={50} round={true} /></li>
+                    </a>
+                    <a href={`https://facebook.com`}
+                        target={"_blank"} rel="noreferrer">
+                        <li><FacebookIcon size={50} round={true} /></li>
+                    </a>
+                    <a href={`https://pinterest.com`}
+                        target={"_blank"} rel="noreferrer">
+                        <li><PinterestIcon size={50} round={true} /></li>
+                    </a>
+                    <a href={`https://telegram.org/`}
+                        target={"_blank"} rel="noreferrer">
+                        <li><TelegramIcon size={50} round={true} /></li>
+                    </a>
+                    <a href={'https://twitter.com'}
+                        target={"_blank"} rel="noreferrer">
+                        <li><TwitterIcon size={50} round={true} /></li>
+                    </a>
+                </ul>
                 <div className="row">
                     {algoApiData.movies?.map((item) => (
                         <div className='list-movie-item' key={item.imdbID}>
